de619d733260abaccad669f0615ea5ccb3d3c7c9
Maxim Hasanau <maxlinus1408@gmail.com>
2021-10-02 10:07:28 +0300

    fix: fix errors

--- js/main.js
+++ js/main.js
@@ -1,50 +1,52 @@
-function go() {
-    alert('Does not have yet.')
-  }
-  (function () {
-  
-    const smoothScroll = function (targetEl, duration) {
-        const headerElHeight =  document.querySelector('.header').clientHeight;
-        let target = document.querySelector(targetEl);
-        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
-        let startPosition = window.pageYOffset;
-        let startTime = null;
-    
-        const ease = function(t,b,c,d) {
-            t /= d / 2;
-            if (t < 1) return c / 2 * t * t + b;
-            t--;
-            return -c / 2 * (t * (t - 2) - 1) + b;
-        };
-    
-        const animation = function(currentTime){
-            if (startTime === null) startTime = currentTime;
-            const timeElapsed = currentTime - startTime;
-            const run = ease(timeElapsed, startPosition, targetPosition, duration);
-            window.scrollTo(0,run);
-            if (timeElapsed < duration) requestAnimationFrame(animation);
-        };
-        requestAnimationFrame(animation);
-  
+function scroll() {
+  alert('Does not have yet.');
+}
+
+(function () {
+  const smoothScroll = function (targetEl, duration) {
+    let headerElHeight = document.querySelector('.header').clientHeight;
+    let target = document.querySelector(targetEl);
+    let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+    let startPosition = window.pageYOffset;
+    let startTime = null;
+
+    const ease = function (t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
     };
-  
-    const scrollTo = function () {
-        const links = document.querySelectorAll('.js-scroll');
-        links.forEach(each => {
-            each.addEventListener('click', function () {
-                const currentTarget = this.getAttribute('href');
-                smoothScroll(currentTarget, 1000);
-            });
-        });
+
+    const animation = function (currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, targetPosition, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
     };
-    scrollTo();
-  }());
+
+    requestAnimationFrame(animation);
+  };
+
+  const scrollTo = function () {
+    const links = document.querySelectorAll('.js-scroll');
+    links.forEach(each => {
+      each.addEventListener('click', function () {
+        const currentTarget = this.getAttribute('href');
+        smoothScroll(currentTarget, 1000);
+      });
+    });
+  };
+
+  scrollTo();
+})();
 
 function go() {
-  let value = document.querySelector('.team');
-  value.style.background = 'linear-gradient(90deg, #f598a8, #f6edb2)';
-} 
+  const value1 = document.querySelector('.team');
+  value1.style.background = 'linear-gradient(90deg, #f598a8, #f6edb2)';
+}
+
 function go_away() {
-  let value = document.querySelector('.team');
-  value.style.background = '#EEEEEE';
+  const value2 = document.querySelector('.team');
+  value2.style.background = '#EEEEEE';
 }
